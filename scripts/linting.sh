63e2af3635b74ead8cb736351703f418be68c2b4
juapatral <juanptrujilloalviz@gmail.com>
2021-11-25 19:02:14 -0500

    Added print options

--- scripts/linting.sh
+++ scripts/linting.sh
@@ -1,14 +1,19 @@
 # Black
+ECHO Running black...
 black src/
 
 # Flake
+ECHO Running flake...
 flake8 src/
 
 # Isort
+ECHO Running isort...
 isort src/
 
 #Pydocstyle
+ECHO Running pydocstyle...
 pydocstyle src/
 
 # Mypy
+ECHO Running mypy...
 mypy src/
